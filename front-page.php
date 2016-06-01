c99bb140a01b8b24e80d1389664ab40e4f2b8c3d
Simon Merrick <s.m3rrick@gmail.com>
2016-06-01 17:19:14 +1200

    saving changes pushing to mac

--- front-page.php
+++ front-page.php
@@ -1,5 +1,5 @@
 <?php get_header();?>
-  
+
   <div class="media-banner">
     <video class="media-banner__video" poster="<?php echo get_template_directory_uri();?>/assets/images/bg-home.jpg" autoplay loop>
       <source src="<?php echo get_template_directory_uri();?>/assets/images/bg-home.m4v" type="video/mp4">
@@ -20,22 +20,32 @@
       <span class="introduction__kiwi">New Zealand Water Sports</span>
     </div>
 
-    <a class="discover">Discover More Below</a>
   </div>
 
-  <div class="row" style="padding: 15px 0px">
-    <div class="medium-6 column text-right">
-      <h1>BugPolo</h1>
-      <p style="font-size: 1.25em;">Derived from water polo and evolved around the River Bug, BugPolo is a homegrown pool sport everyone can enjoy.</p>
-      <a class="button" href="bugpolo">Get on the pool</a>
+  <section style="margin-bottom: 50px;" class="medium-collapse">
+
+    <div class="row">
+      <div class="medium-6 columns text-right" style="margin-top: 50px;">
+        <h3 style="color: #22b8eb;">On the Pool</h3>
+        <p style="font-size: 1.1em; color: #7A8491">Derived from water polo and evolved around the River Bug, BugPolo is a homegrown pool sport everyone can enjoy.</p>
+        <a class="" href="bugpolo" style="color: #FF992E;">« Discover the joy of BugPolo</a>
+      </div>
+
+      <div class="medium-6 columns" style="margin-top: 50px;">
+        <h3 style="color: #22b8eb;">In the River</h3>
+        <p style="font-size: 1.1em; color: #7A8491">From breathtaking scenery to thrilling rapids, River Bugging is the perfect way to experience New Zealand rivers.</p>
+        <a class="" href="river-bugging" style="color: #FF992E;">Experience River Bugging »</a>
+      </div>
     </div>
 
-    <div class="medium-6 column">
-      <h1>River Bugging</h1>
-      <p style="font-size: 1.25em;">From breathtaking scenery to thrilling rapids, River Bugging is the perfect way to experience New Zealand rivers.</p>
-      <a class="button" href="river-bugging">Get on the river</a>
-    </div>
-  </div>
+  </section>
+
+  <div style="float: none; clear: both;"></div>
+
+  <div class="parallax action"></div>
+
+  <div class="parallax adventure"></div>
+  
+  <div style="float: none; clear: both;"></div>
 
 <?php get_footer();?>
-
