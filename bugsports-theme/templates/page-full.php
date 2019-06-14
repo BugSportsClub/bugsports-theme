061d54b1ce27df2203b925561024409b91eb037f
Simon Merrick <simonmerrick@catalyst.net.nz>
2019-06-14 16:40:00 +1200

    tweeking previous styling

--- bugsports-theme/templates/page-full.php
+++ bugsports-theme/templates/page-full.php
@@ -7,13 +7,11 @@ get_header(); ?>
 <?php get_template_part( 'parts/featured-image' ); ?>
 
 <div class="row">
-	<div class="small-12 large-12 columns" role="main">
 
-	<?php /* Start loop */ ?>
 	<?php while ( have_posts() ) : the_post(); ?>
 		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
 			<header>
-				<h1 class="entry-title"><?php the_title(); ?></h1>
+				<h1 class="entry-title text-center"><?php the_title(); ?></h1>
 			</header>
 			<div class="entry-content">
 				<?php the_content(); ?>
@@ -26,7 +24,6 @@ get_header(); ?>
 		</article>
 	<?php endwhile; // End the loop ?>
 
-	</div>
 </div>
 
 <?php get_footer(); ?>
