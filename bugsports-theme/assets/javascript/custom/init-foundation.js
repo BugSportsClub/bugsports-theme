4ddc98419d7f0432fe9ed1af45d7baad8166f7a5
Simon Merrick <simon.merrick@outlook.co.nz>
2019-10-28 18:19:40 +1300

    Remove duplicate files

--- bugsports-theme/assets/javascript/custom/init-foundation.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).foundation();
-
-jQuery(document).on('open.fndtn.offcanvas', '[data-offcanvas]', function () {
-  jQuery('html').css('overflow', 'hidden');
-});
-
-jQuery(document).on('close.fndtn.offcanvas', '[data-offcanvas]', function () {
-  jQuery('html').css('overflow', 'auto');
-});
